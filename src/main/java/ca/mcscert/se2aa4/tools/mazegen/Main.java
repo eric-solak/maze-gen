a24caeef3898023b514af60c43bb400b6d8d4550
eric-solak <solakeric28@gmail.com>
2024-09-05 14:15:24 -0400

    autoCommit 14:15:24.35

--- src/main/java/ca/mcscert/se2aa4/tools/mazegen/Main.java
+++ src/main/java/ca/mcscert/se2aa4/tools/mazegen/Main.java
@@ -5,7 +5,6 @@ import java.util.Random;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-
 public class Main {
 
     private static final Logger logger = LogManager.getLogger();
